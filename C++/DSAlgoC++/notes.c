2f67f2c438dbbb55fc462298228ef484b4063043
Sai Ganesh <shankarsaiganesh@gmail.com>
2020-06-11 00:21:22 +0800

    Update to C++ files

---
+++ C++/DSAlgoC++/notes.c
@@ -0,0 +1,18 @@
+// Data structure is a way in which data is stored on a computer
+    // in a specific manner
+    // efficient data search and retrieval
+    // specific for specific problems
+    // manage large amount of data such as large databases and indexing services such as hash table
+
+    // Array is collection of elements of same type placed in contiguous memory locations that can be individually referenced by using an
+    // index to an unique identifier
+    // int numbers[4] = {10, 20 ,30 ,40}
+
+    // Abstract data types are entities that are definitions of data and operations but do not have implementation details
+
+// Algorithms is a process or set of rules to be followed in calculations that are carried in a specific
+    //  inputs: 0 or more input values
+    //  outputs: 1 or more than 1 output
+    //  Unambiguity: clear and simple instructions
+    //  Finiteness: Limited number of instructions
+    //  Effectiveness: Each instruction has an impact on the overall process
